79078a6de63469640649bf7f0161dd81634621ac
Hamada Nobuo <nobu0sys@gmail.com>
2026-02-12 09:11:17 +0900

    不要ファイルの削除

--- src.ExprParser- コピー/Expr/ExprExtensions.hs
+++
@@ -1,9 +0,0 @@
-module Expr.ExprExtensions (exprWithDo) where
-
-import Expr.DoParser (doExpr)
-import Expr.ExprParser (expr)
-
-exprWithDo :: Parser Expr
-exprWithDo =
-  try doExpr
-    <|> expr
--- src.型推論HMの途中/AST/Program.hs
+++
@@ -1,8 +0,0 @@
-module AST.Program where
-
-
-data Program = Program
-  { moduleDecl :: Maybe Decl
-  , imports    :: [Decl]
-  , decls      :: [Decl]
-  }
